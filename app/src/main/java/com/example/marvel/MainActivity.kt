a3a060fba19a29349d15d80292d4c24b7e0479c4
FernanAlv <fernandoalvarez07152004@gmail.com>
2024-12-07 20:45:48 -0600

    Nada

--- app/src/main/java/com/example/marvel/MainActivity.kt
+++ app/src/main/java/com/example/marvel/MainActivity.kt
@@ -9,7 +9,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+
 import com.example.marvel.ui.theme.MarvelTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,12 +35,4 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MarvelTheme {
-        Greeting("Android")
-    }
 }
